451e23e6b875dd53c02c82f48f6742fa4ba6cd71
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-10-08 13:56:09 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function addingEventListener() {
 
-    function addingEventListener() {}
 
     const input = document.getElementById('input');
     input.addEventListener('click', function(event) {
